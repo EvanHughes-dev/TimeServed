8642c5f1d5c1ab9e6675ceead7a26167b0fda62e
JamesY231 <jjy1517@rit.edu>
2025-03-05 21:23:43 -0500

    Buttons are loaded

---
+++ Button.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeEveryDayRecount
+{
+    class Button
+    {
+        //Fields
+        private Texture2D _image;
+        private Texture2D _hoverImage;
+        private Rectangle _rectangle;
+
+        public event Action OnClick;
+
+        //Constructor
+        public Button(Texture2D image, Texture2D hoverImage, Rectangle rectangle)
+        {
+            _image = image;
+            _hoverImage = hoverImage;
+            _rectangle = rectangle;
+        }
+
+        //Methods
+
+        /// <summary>
+        /// Draws the button, with the image changing when hovered over
+        /// </summary>
+        /// <param name="sb">sprite batch used for drawing the button</param>
+        public void Draw(SpriteBatch sb)
+        {
+            MouseState ms = Mouse.GetState();
+            if (ms.X >= _rectangle.Left && ms.X <= _rectangle.Right &&
+                ms.Y >= _rectangle.Top && ms.Y <= _rectangle.Bottom)
+            {
+                sb.Draw(_hoverImage, _rectangle, Color.White);
+            }
+
+            else
+            {
+                sb.Draw(_image, _rectangle, Color.White);
+            }
+        }
+
+        public void Click()
+        {
+            OnClick?.Invoke();
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace MakeEveryDayRecount
 {
@@ -21,6 +23,14 @@ namespace MakeEveryDayRecount
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        //Buttons
+        private List<Button> pauseButtons;
+        private List<Button> menuButtons;
+        private Texture2D defaultButtonTexture;
+
+
+        private GameState _state;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -30,7 +40,9 @@ namespace MakeEveryDayRecount
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            //Initialize button lists
+            pauseButtons = new List<Button>();
+            menuButtons = new List<Button>();
 
             base.Initialize();
         }
@@ -39,13 +51,56 @@ namespace MakeEveryDayRecount
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            //Load buttons
+            LoadButtons();
+
+
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            switch (_state)
+            {
+                case GameState.Menu:
+                    //If start game button is pressed
+                    //_state = GameState.Level;
+                    break;
+
+                case GameState.Pause:
+                    //If quit button is pressed
+                    //_state = GameState.Menu;
+
+                    //If unpaused button is pressed
+                    //_state = GameState.Level;
+                    break;
+
+                case GameState.Level:
+                    //On level end
+                    //_state = GameState.Cutscene;
+
+                    //If pause button is pressed
+                    //_state = GameState.Pause;
+                    break;
+
+                case GameState.Cutscene:
+                    //When the cutscene is over
+                    //_state = GameState.Level;
+
+                    //If this is the last cutscene
+                    //_state = GameState.Playback;
+                    break;
+
+                case GameState.Playback:
+                    //When playback is finished
+                    //_state = GameState.Menu;
+                    break;
+
+                default:
+                    break;
+            }
 
             // TODO: Add your update logic here
 
@@ -58,11 +113,63 @@ namespace MakeEveryDayRecount
 
             // TODO: Add your drawing code here
 
+
             base.Draw(gameTime);
         }
 
         private void CheckKeyboardInput() { }
         private void DisplayPauseMenu(SpriteBatch sb) { }
         private void DisplayMainMenu(SpriteBatch sb) { }
+
+        /// <summary>
+        /// Creates all buttons and fills their respective lists them with them.
+        /// </summary>
+        private void LoadButtons()
+        {
+            //Load button textures
+            defaultButtonTexture = Content.Load<Texture2D>("Default Button");
+            //... and more, when we have them
+
+            //Fill pause buttons list with buttons
+            Rectangle pauseContinueRect = new Rectangle(200, 200, 400, 100);
+            Button pauseContinue = new Button(defaultButtonTexture, defaultButtonTexture, pauseContinueRect);
+            pauseContinue.OnClick += MakeSwitchStateAction(GameState.Level);
+            pauseButtons.Add(pauseContinue);
+
+            Rectangle pauseLastCheckpointRect = new Rectangle(200, 320, 400, 100);
+            Button pauseLastCheckpoint = new Button(defaultButtonTexture, defaultButtonTexture, pauseLastCheckpointRect);
+            //pauseLastCheckpoint += the method that brings you back to your last checkpoint
+            pauseButtons.Add(pauseLastCheckpoint);
+
+            Rectangle pauseQuitRect = new Rectangle(200, 440, 400, 100);
+            Button pauseQuit = new Button(defaultButtonTexture, defaultButtonTexture, pauseQuitRect);
+            pauseQuit.OnClick += MakeSwitchStateAction(GameState.Menu);
+            pauseButtons.Add(pauseQuit);
+
+            //Fill menu buttons list with buttons
+            Rectangle menuPlayRect = new Rectangle(400, 200, 600, 200);
+            Button menuPlay = new Button(defaultButtonTexture, defaultButtonTexture, menuPlayRect);
+            menuPlay.OnClick += MakeSwitchStateAction(GameState.Level);
+            menuButtons.Add(menuPlay);
+
+            Rectangle menuQuitRect = new Rectangle(400, 440, 300, 100);
+            Button menuQuit = new Button(defaultButtonTexture, defaultButtonTexture, menuPlayRect);
+            //menuQuit.OnClick += the method that closes the game
+            menuButtons.Add(menuQuit);
+
+        }
+
+        /// <summary>
+        /// Creates an Action delegate to set the state to the given GameState. DOES NOT CHANGE THE STATE ITSELF!
+        /// </summary>
+        /// <param name="state">The state that should be applied when the delegate is called.</param>
+        /// <returns>An Action that sets the game state to the provided state when the ACTION is called.</returns>
+        public Action MakeSwitchStateAction(GameState state)
+        {
+            return () =>
+            {
+                _state = state;
+            };
+        }
     }
 }
