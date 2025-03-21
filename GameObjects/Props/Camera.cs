7d21a5fb1ed998a151710910465b15f913ad78db
Daniel Murphy <dtm6103@rit.edu>
2025-03-21 15:48:31 -0400

    Started coding the cameras and making method stubs.

--- GameObjects/Props/Camera.cs
+++ GameObjects/Props/Camera.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -6,8 +7,38 @@ namespace MakeEveryDayRecount.GameObjects.Props
 {
     internal class Camera : Prop
     {
+        //Specify a line for the center of it's vision and the length of its vision, an angle in radians for the width of it's field of vision
+        private bool _active;
+
+        //The camera will get a certain set of rays when it's created, and 
+        private Vector2[] _rays;
+
+        private List<Point> _watchedTiles;
+
         public Camera(Point location, Texture2D sprite)
-            : base(location, sprite) { }
+            : base(location, sprite)
+        {
+            _watchedTiles = new List<Point>();
+            //All cams start active
+            _active = true;
+        }
+
+        public void Update(float deltaTime)
+        {
+            //Add a check to see if there's a box directly in front of the camera, and if so don't check any of the rays
+        }
+
+        private void CheckRay(Vector2 ray)
+        {
+            //create a parametric line using the point and the vector
+            //Step along that line in small intervals, checking what tile you're over at every step
+        }
+
+        private void LookForPlayer()
+        {
+            throw new NotImplementedException();
+            //This can be optimized by making it only check the tiles on the outside of the "vision cone"
+        }
 
         public override void Interact(Player player)
         {
