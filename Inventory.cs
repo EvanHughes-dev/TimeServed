982eb703b2437895b002c3d7e363cd37c4fddf0e
Avigail <GuliD@GULIDEE>
2025-03-21 13:20:56 -0400

    updated constructor and draw

--- Inventory.cs
+++ Inventory.cs
@@ -7,30 +7,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MakeEveryDayRecount.GameObjects.Props;
 
 namespace MakeEveryDayRecount
 {
     internal class Inventory
     {
 
-        //inventory will always be 4 buttons
-        //Button[] inventoryUI = new Button[4];
+        /// <summary>
+        /// Array of buttons used to display the inventory
+        /// </summary>
+        private Button[] _inventoryUI = new Button[4];
+
+        /// <summary>
+        /// A list of the items the inventory currently holds
+        /// </summary>
         private List<Item> _contents;
+
+        /// <summary>
+        /// Returns the list of the items currently in the inventory
+        /// </summary>
         public List<Item> Contents { get { return _contents; } }
 
         public Inventory()
         {
-            //for (int i = 0; i < inventory.Length; i++)
-            //{
-            //    create a button and offset the x
-            //}
+            for (int i = 0; i < _inventoryUI.Length; i++)
+            {
+                //create a button and offset the x
+                _inventoryUI[i] = new Button(AssetManager.InventoryBoxes[0], AssetManager.InventoryBoxes[1],
+                    new Rectangle(0 + (AssetManager.InventoryBoxes[0].Width * i), 0,
+                    AssetManager.InventoryBoxes[0].Width, AssetManager.InventoryBoxes[0].Height), true);
+            }
         }
         public void Draw(SpriteBatch sb) 
         {
-            //for each(Buton button in inventoryUI)
-            //{
-            //    button.Draw()
-            //}
+            foreach (Button button in _inventoryUI)
+            {
+                button.Draw(sb);
+            }
 
         }
     }
