a113767aa76dd75da4de241368ac05afd49945ed
Daniel Murphy <dtm6103@rit.edu>
2025-03-23 17:23:22 -0400

    Made a constructor for the camera and added a checkray function for one octet :)

--- GameObjects/Props/Camera.cs
+++ GameObjects/Props/Camera.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -14,28 +15,68 @@ namespace MakeEveryDayRecount.GameObjects.Props
         //The ray at the center of the vision cone
         private Vector2 _centerRay;
         //The camera will get a certain set of rays when it's created, and those rays will never change, even if they get blocked
+        //TODO: Maybe these should be changed into points?
         private Vector2[] _rays;
+
+        private float _spread;
 
         private List<Point> _watchedTiles;
 
-        public Camera(Point location, Texture2D sprite)
+        /// <summary>
+        /// Makes a security camera that watches a certain vision cone to see if the player is inside it
+        /// </summary>
+        /// <param name="location">The location of the camera, from which the vision cone is projected</param>
+        /// <param name="sprite">The camera's sprite</param>
+        /// <param name="centerRay">The ray that forms the center of the camera's vision cone</param>
+        /// <param name="spread">The arc from the center of the vision cone to the edge, in radians</param>
+        public Camera(Point location, Texture2D sprite, Vector2 centerRay, float spread)
             : base(location, sprite)
         {
             _watchedTiles = new List<Point>();
             //All cams start active
             _active = true;
+            _centerRay = centerRay;
+            _spread = spread;
         }
         //TODO: add an alternative constructor that takes a point as the center of the vision cone and constructs a vector from that
 
         public void Update(float deltaTime)
         {
             //Add a check to see if there's a box directly in front of the camera, and if so don't check any of the rays
+            CheckRay(new Vector2(5, 4));
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+
         }
 
         private void CheckRay(Vector2 ray)
         {
             //create a parametric line using the point and the vector
             //Step along that line in small intervals, checking what tile you're over at every step
+            float dx = ray.X;
+            float dy = ray.Y;
+            float D = 2 * dy - dx;
+            int y = Location.Y;
+
+            for (int x = Location.X; x < Location.X + ray.X; x++)
+            {
+                _watchedTiles.Add(new Point(x, y));
+                if (D > 0)
+                {
+                    y++;
+                    D = D - 2 * dx;
+                }
+                if (D == D+ 2* dy)
+                {
+                    break;
+                }
+            }
+            foreach (Point p in _watchedTiles)
+            {
+                Debug.WriteLine(p);
+            }
         }
 
         private void LookForPlayer()
