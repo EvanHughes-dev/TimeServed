107085a3c5c3b614ef7306c8fba96209ae56cd9b
Evan Hughes <ejhingm@comcast.net>
2025-04-07 20:25:42 -0400

    added todo

--- LevelEditor/Forms/NewRoomForm.cs
+++ LevelEditor/Forms/NewRoomForm.cs
@@ -51,7 +51,7 @@ namespace LevelEditor
             if (result == DialogResult.OK)
             {
                 Room room = FileIOHelpers.LoadRoom(openFileDialog.FileName, _mainForm.Tiles, _mainForm.Props);
-
+                // TODO figure out room index for this
                 _mainForm.AddNewRoom(room);
 
                 EditorForm editor = new(_mainForm, room);
