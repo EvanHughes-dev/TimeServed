dd1c0f69cba8939ac3b14fab168c8ef28c122c0d
Evan Hughes <ejhingm@comcast.net>
2025-04-09 14:37:16 -0400

    xml comment update

--- LevelEditor/Classes/Props/Door.cs
+++ LevelEditor/Classes/Props/Door.cs
@@ -30,15 +30,16 @@ namespace LevelEditor.Classes.Props
         /// Creates a new Door with the given sprite, position, KeyType, and facing direction.
         /// </summary>
         /// <param name="sprite">The sprite this Prop displays with.</param>
-        /// <param name="position">The position of this Prop.</param>
-        /// <param name="imageIndex"> Index of this image</param>
         /// <param name="keyToOpen">
         ///     The type of key that can be used to open this door. 
         ///     Set to None to make this door unlocked.
         /// </param>
+        /// <param name="position">The position of this Prop.</param>
+        /// <param name="imageIndex"> Index of this image</param>     
         /// <param name="destination">Destination the player will be at when they interact</param>
-        public Door(Image sprite, int imageIndex, KeyType keyToOpen, Point? position=null, Point? destination = null, int? roomIndex=null)
-            : base(sprite,  imageIndex, ObjectType.Door, position)
+        /// <param name="roomIndex"> Index of the room this door leads to</param>
+        public Door(Image sprite, int imageIndex, KeyType keyToOpen, Point? position = null, Point? destination = null, int? roomIndex = null)
+            : base(sprite, imageIndex, ObjectType.Door, position)
         {
             // Save params
             KeyToOpen = keyToOpen;
@@ -54,7 +55,7 @@ namespace LevelEditor.Classes.Props
         /// <returns>A copy of this Door at the given position.</returns>
         public override Door Instantiate(Point position)
         {
-            return new Door(Sprite, ImageIndex, KeyToOpen,  position);
+            return new Door(Sprite, ImageIndex, KeyToOpen, position);
         }
 
         /// <summary>
