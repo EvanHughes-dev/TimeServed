50771d6e02cb4ef5793289a0403024fdc5111ca4
Leah-Crain <100484500+Leah-Crain@users.noreply.github.com>
2025-04-27 15:39:47 -0400

    Create maze level

--- Game1.cs
+++ Game1.cs
@@ -74,7 +74,7 @@ namespace MakeEveryDayRecount
             //_graphics.PreferredBackBufferHeight = 360;
 
             _graphics.HardwareModeSwitch = false;
-            _graphics.IsFullScreen = true;
+            _graphics.IsFullScreen = false; // UNDO THIS CHANGE. DO NOT LET THIS REACH MAIN
             _graphics.ApplyChanges();
 
             _debugState = DebugState.None;
